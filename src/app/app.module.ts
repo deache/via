df8677263730f9b3508f0d490f7eb1f176b17788
Damaris Brizeth Puente Tamez <damaris.puente@neoris.cxnetworks.net>
2018-11-03 14:43:32 -0600

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from 'src/app/components/introduction/introduction.component';
+import { CompareComponent } from 'src/app/components/compare/compare.component';
+
+const appRoutes: Routes = [
+  { path: 'introduction', component: IntroductionComponent },
+  { path: 'compare',      component: CompareComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent,
+    CompareComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
