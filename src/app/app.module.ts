2c7b207a9b984311e4961c31f311744a1fc219c7
deache <de.hernandez93@gmail.com>
2018-11-03 14:51:57 -0600

    container in app component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { IntroductionComponent } from 'src/app/components/introduction/introduction.component';
 import { CompareComponent } from 'src/app/components/compare/compare.component';
+import { ExplorationComponent } from './components/exploration/exploration.component';
 
 const appRoutes: Routes = [
   { path: 'introduction', component: IntroductionComponent },
   { path: 'compare',      component: CompareComponent },
+  { path: 'exploration',      component: ExplorationComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroductionComponent,
+    ExplorationComponent,
     CompareComponent
   ],
   imports: [
---
+++ src/app/components/exploration/exploration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-exploration',
+    templateUrl: './exploration.component.html',
+    styleUrls: ['./exploration.component.scss']
+})
+
+export class ExplorationComponent {
+    constructor() {}
+}
